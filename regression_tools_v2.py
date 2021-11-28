afc8e06091efa565d38b79cee34f0590586071da
DaxxC <62670774+DaxxC@users.noreply.github.com>
2021-11-28 15:39:28 -0700

    Update regression_tools_v2.py

--- regression_tools_v2.py
+++ regression_tools_v2.py
@@ -14,6 +14,27 @@ import datetime
 import sklearn
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def binary_metrics_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+  -----
+  y_true = true labels in the form of a 1D array
+  y_pred = predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
+
 def model_early_stopping_callback(metric='val_loss', delta=0, impatience=0, verbosity= 0, best_weights=False):
   """
   Returns a Tensorflow callback for early stopping of a model in training. Metric is the
