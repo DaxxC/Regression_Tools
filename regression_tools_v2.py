2a31cfe54c1ee9ee4dc917672d9dbede5c5d8d91
DaxxC <62670774+DaxxC@users.noreply.github.com>
2021-12-11 16:33:40 -0700

    Update regression_tools_v2.py

--- regression_tools_v2.py
+++ regression_tools_v2.py
@@ -26,6 +26,24 @@ def prediction_timer(model, samples):
   time_per_pred= total_time/len(samples)
   return total_time, time_per_pred
 
+def metrics_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
+
 def binary_metrics_results(y_true, y_pred):
   """
   Calculates model accuracy, precision, recall and f1 score of a binary classification model.
